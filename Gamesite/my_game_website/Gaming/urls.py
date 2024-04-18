19b383e8a595cc796fa6c7e35c76ff7923edf37b
Almantas Valiūnas <almis0505@gmail.com>
2024-04-18 12:00:13 +0300

    Flappy Fly

--- Gamesite/my_game_website/Gaming/urls.py
+++ Gamesite/my_game_website/Gaming/urls.py
@@ -4,4 +4,6 @@ from .views import welcome
 
 urlpatterns = [
     path('', welcome, name='welcome'),
+    path('about/', views.about, name='about'),
+    path('games/', views.games, name='games'),
 ]
--- Gamesite/my_game_website/Gaming/views.py
+++ Gamesite/my_game_website/Gaming/views.py
@@ -7,3 +7,9 @@ from django.http import HttpResponse
 
 def welcome(request):
     return render(request, 'welcome.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def games(request):
+    return render(request, 'games.html')
